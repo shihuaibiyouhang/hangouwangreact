c2f23bc61858753188d4f03367efba3368dab90a
肖军 <jun.xiao@patpat.com>
2023-11-04 12:23:59 +0800

    test:commit

--- hgwreact/src/App.js
+++ hgwreact/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.scss';
-console.log(111)
+console.log(1112)
 class App extends Component {
   render() {
     return (
